7d5c9c6e9c42606f7d528a7ead8fa5232bdc0827
Pin del Rio <Pindelrio@gmail.com>
2024-12-05 10:01:03 +0100

    Textures

--- ComputacionGL/main.cpp
+++ ComputacionGL/main.cpp
@@ -1,58 +1,49 @@
+#include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <iostream>
-#include <windows.h>
+// #include "Shaders/Shader.h"
+// #include "Shaders/ShaderProgram.h"
+#include <STB/stb_image.h>
 
-#include "Shaders/Shader.h"
-#include "Shaders/ShaderProgram.h"
-
-int alternativeColorLocation;
+int customColor;
 float vertices[] = {
-        
-    -0.6f, 0.1f, 0.0f,//A 0
-    -0.5f, 0.3f, 0.0f,//B 1
-    -0.2f, 0.5f, 0.0f,//C 2
-    0.2f, 0.5f, 0.0f, //D 3
-    0.5f, 0.3f, 0.0f, //E 4
-    0.6f, 0.1f, 0.0f, //F 5
-    0.0f, -0.2f, 0.0f,//G 6 //Morro
-    -0.5f, 0.6f, 0.0f,//H 7
-    0.5f, 0.6f, 0.0f, //I 8
-    -0.2f, 0.1f, 0.0f,//J 9
-    0.2f, 0.1f, 0.0f, //K 10
-    -0.3f, 0.0f, 0.0f,//L 11
-    0.3f, 0.0f, 0.0f, //M 12
-
-    -0.11f,0.2f,0.0f, //N 13
-    0.11f,0.2f,0.0f, //O 14
-
-    -0.08f,0.1f,0.0f, //P 15
-    0.08f,0.1f,0.0f    //Q 16
+    //Coordenades      //Color            //Textura
+    -0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, //Top Left
+    -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, //Down Left
+     0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, //Down Right
+     0.5f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, //Top Right
 };
 
+const char* vertexShaderSource =
+"#version 330 core\n"
+"layout (location = 0) in vec3 aPos;  \n"
+"layout (location = 1) in vec3 aColor;\n"
+"layout (location = 2) in vec2 aText; \n"
+"                                     \n"
+"out vec3 ourColor;                   \n"
+"out vec2 textCoord;                  \n"
+"                                     \n"
+"void main()                          \n"
+"{                                    \n"
+"    gl_Position = vec4(aPos, 1.0);   \n"
+"    ourColor = aColor;               \n"
+"    textCoord = aText;               \n"
+"}                                    \n\0";
+
+const char* fragmentShaderSource =
+"#version 330 core                            \n"
+"out vec4 FragColor;                          \n"
+"in vec3 ourColor;                            \n"
+"in vec2 textCoord;                            \n"
+"uniform sampler2D u_texture;                 \n"
+"void main()                                  \n"
+"{                                            \n"
+"    FragColor = texture(u_texture, textCoord);\n"
+"}                                            \n\0";
+
 unsigned int indices[] = {
-    7,1,2, //ORELLA E
-    3,8,4,
-    0,1,11, //GALTA
-    5,4,12,
-    11,6,9,
-    6,10,12,
-
-    1,2,9,
-    3,4,10,
-
-    1,11,9,
-    10,4,12,
-
-    2,3,6,
-    2,9,13,
-    3,10,14,
-
-    2,9,13,
-    9,15,6,
-
-    3,14,10,
-    10,16,6
+    0,1,2,
+    0,2,3
 };
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
@@ -60,25 +51,14 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     glViewport(0, 0, width, height);
 }
 
-float colores[] =
+void processInput(GLFWwindow* window)
 {
-    1.0f, 1.0f, 1.0f, 1.0f,
-    1.0f, 0.0f, 1.0f, 1.0f,
-    1.0f, 1.0f, 0.0f, 1.0f,
-    0.0f, 0.5f, 1.5f, 1.0f
-};
-void Draw(int& i, int& j)
-{
-    glUniform4f(alternativeColorLocation, colores[i+0], colores[i+1], colores[i+2], colores[i+3]);
-    glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, (int*)NULL + (3 * j));
-    i=i+4;
-    if (i>=16)
-        i=0;
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
 
 int main(void)
 {
-   
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -95,7 +75,7 @@ int main(void)
 
     glfwMakeContextCurrent(window);
 
-    //Glad
+    //GLAD
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
@@ -104,10 +84,20 @@ int main(void)
 
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    Shader* MyShader = new Shader();
-    unsigned int shaderProgram = ShaderProgram().CreateAttachAndLinkShaderProgram(MyShader->vertexShader, MyShader->fragmentShader);
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
     
-    alternativeColorLocation = glGetUniformLocation(shaderProgram, "alternativeColor");
+    //customColor = glGetUniformLocation(shaderProgram, "ourColor");
     
     unsigned int VBO; //VertexBufferObject (VBO)
     unsigned int VAO; //VertexAttributeObject
@@ -127,31 +117,71 @@ int main(void)
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    //Triangles
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    
+    unsigned int stride = 8 * sizeof(float);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, (void*)0);
     glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, stride, (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, stride, (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
     
     //Unbindear Buffer
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
    
+    //TEXTURAS
+    //------------------------------------------------------------------------------------------------------------------
+    int widthImg, heighImg, PixelNum;
+
+    //stbi_set_flip_vertically_on_load(true);
+    unsigned char* image = stbi_load("Image.jpg", &widthImg, &heighImg, &PixelNum, 0);
+
+    GLuint textureID;
+    glGenTextures(1, &textureID); //numero de textures
+    glBindTexture(GL_TEXTURE_2D, textureID);
+
+    //Com es comporta el suavitzat de entre pixels
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    if (image)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, widthImg, heighImg, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+    }
+    else
+    {
+        std::cout << "Failed to load image" << std::endl;
+    }
+    stbi_image_free(image);
+    glBindTexture(GL_TEXTURE_2D, 0); //Evitem que la imatge es modifiqui
+
+    //UNIFORM
+    GLuint texUniform = glGetUniformLocation(shaderProgram, "u_texture");
+    glUseProgram(shaderProgram);
+    glUniform1i(texUniform, 0);
     
-    int i = 0;
+    //------------------------------------------------------------------------------------------------------------------
     while (!glfwWindowShouldClose(window))
     {
-        glClearColor(1.0f, 0.9f, 1.0f, 1.0f);
+        glClearColor(1.0f, 0.3f, 1.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glBindTexture(GL_TEXTURE_2D, textureID); //Podem pintem la imatge
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-///---------------------------------
-        // for (unsigned int i = 0; i < sizeof(indices) / sizeof(indices[0]) / 3; i++) {
-        //     glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, (void*)(3 * i * sizeof(GLuint)));
-        // }
-        glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(indices[0]), GL_UNSIGNED_INT, 0);
-///---------------------------------
+        ///---------------------------------
+        for (unsigned int i = 0; i < (sizeof(indices) / sizeof(indices[0])) / 3; i++) {
+            glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, (void*)(3 * i * sizeof(GLuint)));
+        }
+        //glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(indices[0]), GL_UNSIGNED_INT, 0);
+        ///---------------------------------
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
